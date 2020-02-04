82846b71a7c56c5fbc5e5ba15e10478a9af86aba
GuangGuang <lihansey163@163.com>
2020-02-04 18:45:06 +0800

    [Android] support spm 5.0

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
-let package = Package(name: "AndroidSwiftTrace")
+let package = Package(
+        name: "AndroidSwiftTrace",
+        products: [
+            .library(name: "AndroidSwiftTrace", targets: ["trace"])
+        ],
+        targets:[
+            .systemLibrary(name: "trace", path: "Sources"),
+        ]
+)
